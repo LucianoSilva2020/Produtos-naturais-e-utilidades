6b9ee5d9489280804d9cf73b2ccbf826b49c1436
Luciano Silva <93166470+LucianoSilva2020@users.noreply.github.com>
2022-06-22 18:45:06 -0300

    .

--- main.js
+++ main.js
@@ -12,5 +12,200 @@ function openPage(pageName, elmnt, color) {
   elmnt.style.backgroundColor = color;
 }
 
-// Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
+
+
+const produtos = [
+  {
+    id: "1",
+    image: "imagem.png",
+    titulo: "Produto",
+    valor: "R$:230,00"
+  },
+  {
+    id: "2",
+    image: "imagem.png",
+    titulo: "Carro",
+    valor: "R$:300,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+  {
+    id: "3",
+    image: "imagem.png",
+    titulo: "Geladeira",
+    valor: "R$:200,00"
+  },
+
+]
+
+
+
+criaProdutos()
+
+function criaProdutos() {
+  for(let i = 0; i < produtos.length; i++){  
+    const elmDiv = document.createElement('div')
+    const elmImage = document.createElement('img')
+    const elmProduto = document.createElement('p')
+    const elmPreco = document.createElement('p')
+    const elmDivContainer = document.getElementById('flex-container')
+
+    elmImage.setAttribute("src", `imagens/${produtos[i].image}`)
+    elmProduto.innerHTML = produtos[i].titulo
+    elmPreco.innerHTML = produtos[i].valor
+    elmDiv.appendChild(elmImage)
+    elmDiv.appendChild(elmProduto)
+    elmDiv.appendChild(elmPreco)
+    elmDivContainer.appendChild(elmDiv)
+    console.log('aqui')
+  }
+}
